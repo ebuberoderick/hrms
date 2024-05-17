d7f0222c6831a6479d1e75506a8c326b7be28748
ebuberoderick <ebuberoderick2@gmail.com>
2024-05-17 03:10:52 +0100

    added auth hook

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,18 @@
-// 'use client'
+'use client'
 import { Inter } from "next/font/google";
 import 'remixicon/fonts/remixicon.css'
 import "./globals.css";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <body className={inter.className}>{children}</body>
+      </LocalizationProvider>
     </html>
   );
 }
