9e481edf68fb96e5cc8b1a27674a8ddda583de5f
ebuberoderick <ebuberoderick2@gmail.com>
2024-05-17 02:41:40 +0100

    bug fix

--- src/hooks/Auth.js
+++ src/hooks/Auth.js
@@ -1,53 +1,53 @@
-'use client'
-import Cookies from 'js-cookie';
-import { addData } from '../../reduxStore/reducers/UsersReducer';
-import { providerData } from '../../reduxStore/reducers/ProviderStateReducer';
-import { businessData } from '../../reduxStore/reducers/BusinessReducer';
+// 'use client'
+// import Cookies from 'js-cookie';
+// import { addData } from '../../reduxStore/reducers/UsersReducer';
+// import { providerData } from '../../reduxStore/reducers/ProviderStateReducer';
+// import { businessData } from '../../reduxStore/reducers/BusinessReducer';
 
 
-export function SignInAuth(data, dispatch) {
-  // dispatch(addData(data.entity));
-}
+// export function SignInAuth(data, dispatch) {
+//   // dispatch(addData(data.entity));
+// }
 
-export function SignOut(dispatch) {
-  dispatch(addData({}))
-  dispatch(businessData({}))
-  dispatch(providerData(false))
-  Cookies.remove('jwt')
-}
+// export function SignOut(dispatch) {
+//   dispatch(addData({}))
+//   dispatch(businessData({}))
+//   dispatch(providerData(false))
+//   Cookies.remove('jwt')
+// }
 
-export function Session(user) {
-  const session = {
-    status: '',
-    user
-  }
-  if (Object.keys(user.value).length !== 0 && Cookies.get('jwt')) {
-    session.status = verifyJWT(user.value.data.api_token)
-  } else {
-    session.status = 'unauthenticated'
-  }
-  return session
-}
+// export function Session(user) {
+//   const session = {
+//     status: '',
+//     user
+//   }
+//   if (Object.keys(user.value).length !== 0 && Cookies.get('jwt')) {
+//     session.status = verifyJWT(user.value.data.api_token)
+//   } else {
+//     session.status = 'unauthenticated'
+//   }
+//   return session
+// }
 
-export function verifyJWT(jwtToken) {
-  if (jwtToken) {
-    try {
-      const [, payload] = jwtToken.split('.');
-      const { exp: expires } = JSON.parse(window.atob(payload));
-      if (typeof expires === 'number') {
-        const expired = (Date.now() >= expires * 1000)
-        if(expired){
-          return 'unauthenticated'
-        }else{
-          return 'authenticated'
-        }
-      }
-    } catch {
-      return 'unauthenticated'
-    }
-  }
-  return null;
-}
+// export function verifyJWT(jwtToken) {
+//   if (jwtToken) {
+//     try {
+//       const [, payload] = jwtToken.split('.');
+//       const { exp: expires } = JSON.parse(window.atob(payload));
+//       if (typeof expires === 'number') {
+//         const expired = (Date.now() >= expires * 1000)
+//         if(expired){
+//           return 'unauthenticated'
+//         }else{
+//           return 'authenticated'
+//         }
+//       }
+//     } catch {
+//       return 'unauthenticated'
+//     }
+//   }
+//   return null;
+// }
 
 
 
