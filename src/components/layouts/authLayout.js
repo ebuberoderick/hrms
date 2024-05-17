165cefd63b96997ac68e466d151f6571335c60c6
ebuberoderick <ebuberoderick2@gmail.com>
2024-05-17 02:06:40 +0100

    updating auth avatar

--- src/components/layouts/authLayout.js
+++ src/components/layouts/authLayout.js
@@ -2,7 +2,7 @@
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import bg from '@assets/images/authBG.png'
-import avatar from '@assets/images/avatar/img.png'
+import avatar from '@assets/images/avatar/Img.png'
 import React from 'react'
 
 function AuthLayout({children,title,subText,onSubmit}) {
