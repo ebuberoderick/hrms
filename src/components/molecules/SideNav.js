281f967537ec58e3e34b06f82304f543122c1e34
ebuberoderick <ebuberoderick2@gmail.com>
2024-05-20 22:14:11 +0100

    updating side nav

--- src/components/molecules/SideNav.js
+++ src/components/molecules/SideNav.js
@@ -11,8 +11,40 @@ function SideNav() {
       <div className='text-2xl px-1'>
         <Image src={logo} className="h-12 w-12" alt='Michael Michael' />
       </div>
-      <div className='flex-grow flex flex-col gap-2'>
-        <AppLink text={"dashboard"} icon={<i className="ri-home-4-line"></i>} />
+      <div className='flex-grow gap-y-4 flex flex-col gap-2'>
+        <div>
+          <AppLink text={"dashboard"} icon={<i className="ri-home-4-line"></i>} />
+        </div>
+        <div>
+          <div className="font-bold pl-3">SYSTEM ADMINISTRATION</div>
+          <div>
+            <AppLink text={"user"} icon={<i className="ri-user-6-line"></i>} subMenu={[{ name: "users list", extra: false }, { name: "user roles and access", extra: false }, { name: "users last login", extra: false }]} />
+            <AppLink text={"customize setting"} icon={<i className="ri-user-6-line"></i>} subMenu={[{ name: "roles and access", extra: false }, { name: "general settings", extra: false }, { name: "mail setting", extra: false }, { name: "language settings", extra: false }, { name: "variable type", extra: false }, { name: "variable method", extra: false }, { name: "IP settings", extra: false }]} />
+          </div>
+        </div>
+        <div>
+          <div className="font-bold pl-3">PERSONNNEL MANAGEMENT</div>
+          <div>
+            <AppLink text={"recruitment"} icon={<i className="ri-user-6-line"></i>} subMenu={[{ name: "users list", extra: false }, { name: "user roles and access", extra: false }, { name: "users last login", extra: false }]} />
+
+          </div>
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+
+
+
+
+
+
+
+        {/* <AppLink text={"dashboard"} icon={<i className="ri-home-4-line"></i>} />
         <AppLink text={"user"} icon={<i className="ri-user-6-line"></i>} subMenu={[{ name: "users list", extra: false }, { name: "user roles and access", extra: false }, { name: "users last login", extra: false }]} />
         <AppLink text={"employees"} icon={<i className="ri-user-6-line"></i>} subMenu={[{ name: "employees lists", extra: false }, { name: "import employees", extra: false }]} />
         <AppLink text={"customize setting"} icon={<i className="ri-user-6-line"></i>} subMenu={[{ name: "roles and access", extra: false }, { name: "general settings", extra: false }, { name: "mail setting", extra: false }, { name: "language settings", extra: false }, { name: "variable type", extra: false }, { name: "variable method", extra: false }, { name: "IP settings", extra: false }]} />
@@ -28,7 +60,7 @@ function SideNav() {
         <AppLink text={"event & meetings"} icon={<i className="ri-user-6-line"></i>} subMenu={[{ name: "users list", extra: false }, { name: "user roles and access", extra: false }, { name: "users last login", extra: false }]} />
         <AppLink text={"project management"} icon={<i className="ri-user-6-line"></i>} subMenu={[{ name: "users list", extra: false }, { name: "user roles and access", extra: false }, { name: "users last login", extra: false }]} />
         <AppLink text={"support tickets"} icon={<i className="ri-home-4-line"></i>} />
-        <AppLink text={"finance"} icon={<i className="ri-user-6-line"></i>} subMenu={[{ name: "users list", extra: false }, { name: "user roles and access", extra: false }, { name: "users last login", extra: false }]} />
+        <AppLink text={"finance"} icon={<i className="ri-user-6-line"></i>} subMenu={[{ name: "users list", extra: false }, { name: "user roles and access", extra: false }, { name: "users last login", extra: false }]} /> */}
         {/* <AppLink text={"payables"} icon={<i className="ri-money-dollar-circle-fill"></i>} subMenu={[{name:"bills",extra:false},{name:"approvals",extra:false},{name:"payment",extra:false},{name:"procurement",extra:true,option:["projects","RFP","requisition","purchase order","suppliers"]}]} /> */}
       </div>
     </div>
